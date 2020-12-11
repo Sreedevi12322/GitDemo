0233f80c0a56d69fd1061ac3e1018bf76d440c37
sreedevi12322 <sreedevin.kpn@gmail.com>
2020-12-11 17:04:11 +0530

    first commit

---
+++ src/test/java/mavenproject/mvnpjt/AppiumTest.java
@@ -0,0 +1,20 @@
+package mavenproject.mvnpjt;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void Androidtest()
+	{
+		System.out.println("Androidtest");
+		
+	}
+	
+	@Test
+	public void IOsApp()
+	{
+		System.out.println("IOsApp");
+	}
+
+}
---
+++ src/test/java/mavenproject/mvnpjt/RestAssuredTest.java
@@ -0,0 +1,20 @@
+package mavenproject.mvnpjt;
+
+import org.testng.annotations.Test;
+
+public class RestAssuredTest {
+	
+	@Test
+	public void ApiEnable()
+	{
+		System.out.println("ApiEnable");
+		
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
